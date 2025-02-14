e5f06c7ae8eb30ff7c947d61eded81d26a9c1835
Jorge A. Sánchez <jorgesanchezgon251@gmail.com>
2025-02-14 18:42:50 -0500

    Add files via upload

---
+++ 04_operadores.py
@@ -0,0 +1,21 @@
+#Operadores mátematicos
+print(3+4) #suma    
+print(3*4) #multiplicación
+print(3/4) #division
+print(3-4) #resta
+print(10%3) #modulo
+print(10//3) #division con resultado de números enteros
+print(10**3) #potencia
+
+#Operadores para comparar 
+print(3>4) #mayor que
+print(3<4) #menor que
+print(3==4) #igual que
+print(3!=4) #diferente que
+print(3>=4) #mayor igual que
+print(3<=4) #menor igual que 
+
+#Operadores lógicos
+print(3>4 and "hola">"Python") #Realiza bit a bit la operación AND en los operandos 
+print(3>4 or "hola"<"Python") #Realiza bit a bit la operación OR en los operandos
+print(not(3>4)) #Realiza bit a bit la operación XOR en los operandos
