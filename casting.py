8f4f0ebd1e7478d6b0bee723ed7bd27fbb217253
Jorge A. Sánchez <jorgesanchezgon251@gmail.com>
2025-02-18 10:56:52 -0500

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,21 @@
+#--------------------Castear a un entero int()------------------
+new_num = int('1') # -> sera 3 de tipo int
+print(new_num)
+print(type(new_num))
+new_num = int(2.8) # -> sera 2 de tipo int
+print(new_num)
+print(type(new_num))
+#--------------------Castear a un texto str()--------------------
+new_str = str (1)#Sera de tipo str
+print(new_str)
+print(type(new_str))
+nnew_str = str (2.8)#Sera de tipo str
+print(new_str)
+print(type(new_str))
+#----------------------Castear a un float-----------------------
+new_float = float(1) #-> sera 1.0 de tipo float
+print(new_float)
+print(type(new_float))
+new_float = float("2") #-> sera 2.0 de tipo float
+print(new_float)
+print(type(new_float))
