c6d79bcd96749a24fe30908a6c5a6f01e74b36c6
Jorge A. Sánchez <jorgesanchezgon251@gmail.com>
2025-04-02 16:35:13 -0400

    Add files via upload

---
+++ 09_conditionals.py
@@ -0,0 +1,17 @@
+### Conditionals ### 
+ 
+my_condition = 5 * 2
+
+if my_condition > 6: #Si se cumple que 
+    print(my_condition, 'es mayor que 6')
+
+if my_condition == 8:
+    print(my_condition, "es igual que 10")
+else:           #Si no 
+    print("No es igual a 8")
+
+if my_condition == 11 and my_condition > 5: #Podemos tener dos condiciones
+    print("es igual que 10 y mayor que 5")
+elif my_condition != 8: #Funciona de igual manera que un else pero necesita
+    print("Es diferente de 8")
+
